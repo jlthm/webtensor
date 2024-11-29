c35f5ffc9fb3599f42c2291c60d7cfd7b5a8d8e5
jlthm <julianbauer@outlook.at>
2024-11-29 12:29:37 +0100

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -4,18 +4,17 @@ import sys
 import webtensor
 
 __author__ = "jlthm"
-__maintainer__ = "jlthm""
+__maintainer__ = "jlthm"
 __status__ = "Production"
 
 '''
-This console-based application allows recursive web-crawling with level 3 tensors.
-The datatype tensor acts like a multidimensional dictionary.
-Requests are an iteration of the third axis. For detailed information see github @jlthm/webtensor
+This console-based application allows specific and automated recursive webcrawling
+with tensor-datatypes level 3. For detailed information see github @jlthm/webtensor
 
 '''
 
 if not sys.argv[-1].endswith(".py"):
-    print("This script takes no arguments.")
+    print("This module takes no arguments.")
     exit()
 
 console = webtensor.Console("webtensor")
